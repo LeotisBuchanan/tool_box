5f82703ad311da5ff23888bc0d854d8d4fc14a92
Leotis <leotisbuchanan@gmail.com>
2017-04-29 06:32:35 -0400

    add use database to insert.sql

--- scripts/generate_raw_data_insert.py
+++ scripts/generate_raw_data_insert.py
@@ -14,8 +14,9 @@ def format_date(pdte):
    return frmt_dt
 
 
-def main(data_dir):
+def main(data_dir, database):
    sql_template = Template("""INSERT INTO RAW_DATA (pickup_date_time,lat, lon, base) VALUES('$PICKUP_DT', $LAT, $LON, $BASE);""")
+   print("USE uber;")
    data_files_path = glob.glob(data_dir+ "*.csv")
    for path in data_files_path:
       with open(path, 'r') as f:
@@ -38,5 +39,6 @@ def main(data_dir):
 
                            
 if __name__ == "__main__":
-    data_directory =sys.argv[1]                       
-    main(data_directory)
+    data_directory =sys.argv[1]
+    database = sys.argv[2]
+    main(data_directory, database)
