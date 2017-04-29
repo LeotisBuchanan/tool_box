3b5dc17dc98fd03860128c000e8451fe45e40c04
Leotis <leotisbuchanan@gmail.com>
2017-04-29 01:31:08 -0400

    add error check

--- scripts/generate_raw_data_insert.py
+++ scripts/generate_raw_data_insert.py
@@ -14,14 +14,15 @@ def format_date(pdte):
 
 
 def main(data_dir):
-   sql_template = Template("""INSERT INTO RAW_DATA (pickup_date_time,
-   lat, lon, base) VALUES('$PICKUP_DT', $LAT, $LON, $BASE);""")
+   sql_template = Template("""INSERT INTO RAW_DATA (pickup_date_time,lat, lon, base) VALUES('$PICKUP_DT', $LAT, $LON, $BASE);""")
    data_files_path = glob.glob(data_dir+ "*.csv")
    for path in data_files_path:
       with open(path, 'r') as f:
          next(f)
          for line in f:
             fields = line.split(",")
+            if len(fields) < 4:
+               continue
             pickup_dt = format_date(fields[0])
             lat = fields[1]
             lon = fields[2]
@@ -31,8 +32,7 @@ def main(data_dir):
                                     LON=lon,
                                     BASE=base)
             print(insert_sql)
-            import sys
-            sys.exit(0)
+
                            
 if __name__ == "__main__":
     data_directory =sys.argv[1]                       
